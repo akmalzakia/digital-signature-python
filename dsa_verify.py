c03ee4c30391cc050e26b11316fb3835fdb65d48
akmalzakia <zakiasmara11@gmail.com>
2022-10-31 05:17:17 +0700

    done

---
+++ dsa_verify.py
@@ -0,0 +1,84 @@
+import hashlib
+import os
+from random import getrandbits
+from gmpy2 import is_prime
+from gmpy2 import powmod
+import decimal
+
+L = 512
+N = 160
+
+def no_bits(p):
+  return len(bin(p)) - 2
+
+def is_valid(p,q,g):
+  print(no_bits(p), no_bits(q))
+  return (
+    is_prime(p) and is_prime(q) and no_bits(p) == 512 and no_bits(q) == 160 and (p-1) % q == 0 and powmod(g,q,p) == 1 and g > 1
+  )
+
+def display_commands():
+  print("List of commands:")
+  print("1. sign \{document\}")
+  print("2. verify \{document\}")
+
+def main():
+  msg = input()
+  cmd = msg.split(" ")
+
+  try:
+    data = get_file(cmd[0], 'rb')
+  except IndexError:
+    print("Index error")
+    return
+  except FileNotFoundException:
+    print("File not found!")
+    return
+
+  hashed = hashlib.sha256(data).hexdigest()
+  p, q, g = get_dsa_parameters()
+  r = 62848322901515068477839948778542479012575769042
+  s = 17565691724096304496768666585172790092041349629990328096402237941222797906952975133286763510109190664793617507188696160133663882483738644554561
+  y = 4256298120274348371212074726887427396228007773115925395503976054868995480998781161980466575073700741277842431330692929082200233315681317179004680899471810
+  print(verify(hashed, p, q, g, r, s, y))
+
+def get_file(filepath, modes):
+  if os.path.isfile(filepath):
+    f = open(filepath, modes)
+    f_data = f.read()
+    f.close()
+
+    return f_data
+  
+  return FileNotFoundException
+
+def get_dsa_parameters():
+  # print("P value : ")
+  p = 13232376895198612407547930718267435757728527029623408872245156039757713029036368719146452186041204237350521785240337048752071462798273003935646236777459223
+  # print("Q value : ")
+  q = 857393771208094202104259627990318636601332086981
+  # print(p, q)
+  g = 5421644057436475141609648488325705128047428394380474376834667300766108262613900542681289080713724597310673074119355136085795982097390670890367185141189796
+
+  if not is_valid(p, q, g):
+    print('invalid')
+    exit(-1)
+
+  return (p,q,g)
+
+
+def verify(hash, p, q, g, r, s, y):
+  w = pow(s, -1 , q)
+  z = int(hash, 16)
+  u1 = (z * w) % q
+  u2 = (r * w) % q
+  v = ((powmod(g,u1,p) * powmod(y,u2,p)) % p) % q
+  return v == r
+
+class FileNotFoundException(Exception):
+  pass
+
+class InverseErrorException(Exception):
+  pass
+
+main()
---
+++ generate.py
@@ -0,0 +1,86 @@
+import hashlib
+import os
+import rsa
+
+keystore = 'keys/'
+
+def display_commands():
+  print("List of commands:")
+  print("1. sign \{document\}")
+  print("2. verify \{document\}")
+  print("3. generate")
+
+def main():
+  while(True):
+    display_commands()
+    try:
+      msg = input()
+      if not msg:
+        continue
+    except KeyboardInterrupt:
+      break
+
+    cmd = msg.split(" ")
+
+
+    try:
+      data = get_file(cmd[1], 'rb')
+    except IndexError:
+      print("Index error")
+      continue
+    except FileNotFoundException:
+      print("File not found!")
+      continue
+
+    privateKey, publicKey = loadKeys()
+    
+    if cmd[0] == 'sign':
+      sign(cmd[1], data, privateKey)
+    elif cmd[0] == 'verify':
+      print(verify(data, publicKey))
+    else:
+      continue
+
+def get_file(filepath, modes):
+  if os.path.isfile(filepath):
+    f = open(filepath, modes)
+    f_data = f.read()
+    f.close()
+
+    return f_data
+  
+  return FileNotFoundException
+
+def generate_keys():
+  (publicKey, privateKey) = rsa.newkeys(1024)
+  with open('keys/publicKey.pem', 'wb') as p:
+    p.write(publicKey.save_pkcs1('PEM'))
+  with open('keys/privateKey.pem', 'wb') as p:
+    p.write(privateKey.save_pkcs1('PEM'))
+
+def loadKeys():
+  with open('keys/publicKey.pem', 'rb') as p:
+    publicKey = rsa.PublicKey.load_pkcs1(p.read())
+  with open('keys/privateKey.pem', 'rb') as p:
+    privateKey = rsa.PrivateKey.load_pkcs1(p.read())
+
+  return privateKey, publicKey
+
+def sign(filename, data, key):
+  encrypted_hash = rsa.sign(data, key, 'SHA-256')
+  with open('signature.rsa', 'wb') as f:
+    f.write(encrypted_hash)
+  return
+
+def verify(data, key):
+  try:
+    with open('signature.rsa', 'rb') as f:
+      res = rsa.verify(data, f.read(), key)
+    return res
+  except:
+    return False
+
+class FileNotFoundException(Exception):
+  pass
+
+main()
